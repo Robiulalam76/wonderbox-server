7db58e75ff5ddef200ae1f9725a34442d38c4a75
Robiulalam76 <robiulalam762400@gmail.com>
2023-06-26 19:06:21 +0600

    updated env

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-const app = require('./app');
+const app = require("./app");
 
 // connect database
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://wonderbox:762485@cluster0.qcahanl.mongodb.net/', {
-            // useFindAndModify: false,
-            // useCreateIndex: true,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('mongodb connection success!');
-    } catch (err) {
-        console.log('mongodb connection failed!', err.message);
-    }
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      // useFindAndModify: false,
+      // useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("mongodb connection success!");
+  } catch (err) {
+    console.log("mongodb connection failed!", err.message);
+  }
 };
 
-connectDB()
+connectDB();
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
